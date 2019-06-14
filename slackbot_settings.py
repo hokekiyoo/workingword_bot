9f0a09dcbf52b66e24dea3cb10469278b6c55924
hokekiyoo <imslotter1@gmail.com>
2019-06-14 17:52:07 +0900

    init

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定。ここを変える！
+API_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# bot宛メッセージの例外対応
+DEFAULT_REPLY = "HELLO, SOMETHING WRONG"
+
+# プラグイン。この中に呼びかけた後の反応などを書く
+PLUGINS = ['plugins']
