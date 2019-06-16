5770bc0e3bebf4cea6ae207f08336f94e50bf694
hokekiyoo <imslotter1@gmail.com>
2019-06-16 06:54:24 +0000

    改行

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,10 +1,9 @@
 # coding: utf-8
-
 with open("TOKEN.text") as f:
-    TOKEN = f.read()
+    TOKEN = f.readline().split("\n")
 
 # botアカウントのトークンを指定
-API_TOKEN = TOKEN
+API_TOKEN = TOKEN[0]
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "HELLO, SOMETHING WRONG"
